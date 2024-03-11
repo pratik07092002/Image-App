71f726060e6d491d731b9e137f73149be9a24be3
pratik07092002 <pratikgaikwad072002@gmail.com>
2024-03-11 13:36:15 +0530

    python Flask server code

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+app.route('/upload')
+def sendImg():
+    imagefile = request.files['image']
+    imagefile.save('uploads/' + imagefile.filename)
+    
+    return "Image uploaded " , 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
